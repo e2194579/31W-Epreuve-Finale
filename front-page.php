a7899c472d33745608b6c3e9b7268d318d9366ec
Rbc <e2194579@cmaisonneuve.qc.ca>
2022-08-23 16:52:54 -0400

    Modification de front-page (supprimer les post accueil, style.css (changer les couleurs des box des ateliers) et template atelier.php

--- front-page.php
+++ front-page.php
@@ -6,34 +6,9 @@
                         'menu' => 'Sidebar3',
                         'container' => 'nav',
                         'container_class' => 'menu__sidebar menu__atelier',
-                        'menu_class' => 'menu__sidebar__ul'
+                        'menu_class' => 'menu__sidebar__ul',
+                        
             )) ?>
-
-
-
-            
-    <?php
-    if (have_posts()) :
-        while (have_posts()) : the_post(); ?>
-            <?php $lien = "<a class='bouton' href='" . get_permalink() . "'>" . substr(get_the_title(), 0, 8) . "</a>" ?>
-
-
-
-            <h2><?= get_field('titre'); ?></h2>
-
-            <p><?= get_field('sous_titre'); ?></p>
-            <p><?= get_field('description'); ?></p>
-            
-            
-            
-
-
-
-        <?php endwhile; ?>
-    <?php endif; ?>
-
-
-
-    
+              
 </section>
 <?php get_footer(); ?>
--- template-atelier.php
+++ template-atelier.php
@@ -7,8 +7,6 @@
 
 <main class="site__main">
 
-   <?php //the_post_thumbnail("annonce"); ?>
-   <!-- <mark>Événement à venir</mark> -->
        <?php if (have_posts()): the_post(); ?>
       <article class="annonce__article">
          <h1><?= get_the_title() ?></h1>
@@ -26,34 +24,13 @@
          $type = get_field("type");
          $niveau = get_field("niveau");
          
-
-
-
          echo "<p class='annonce__animateur'>Animateur: " . $animateur . "</p>";
-
-       /*   echo "<p>Type de formation: </p>";
-         echo " <input type='radio' id='css' name='a' value='css'> <label for='css'>CSS</label>";
-         echo " <input type='radio' id='php' name='a' value='php'> <label for='php'>PHP</label>";
-         echo " <input type='radio' id='js' name='a' value='js'> <label for='js'>JS</label>"; */
-         echo "<p class='annonce__hs'>Type de formation: " . $type . "</p>";
-
+         echo "<p class='annonce__type'>Type de formation: " . $type . "</p>";
          echo "<p class='annonce__date'>La date: " . $date . "</p>";
          echo "<p class='annonce__duree'>La durée: " . $duree . "</p>";
          echo "<p class='annonce__heure'>Le heure: " . $heure . "</p>";
-
-      /*    echo "<p>Nombre d'heure par séance: </p>";
-         echo " <input type='radio' id='1' name='a' value='1'> <label for='1'>1h</label>";
-         echo " <input type='radio' id='2' name='a' value='2'> <label for='2'>2h</label>";
-         echo " <input type='radio' id='3' name='a' value='3'> <label for='3'>3h</label>"; */
          echo "<p class='annonce__hs'>Nombre d'heure par séance: " . $hs . "</p>";
-
-
-      /*   echo "<p>Niveau de difficulté: </p>";
-         echo " <input type='radio' id='debutant' name='a' value='debutant'> <label for='debutant'>Débutant</label>";
-         echo " <input type='radio' id='intermediaire' name='a' value='intermediaire'> <label for='intermediaire'>Intermédiaire</label>";
-         echo " <input type='radio' id='expert' name='a' value='expert'> <label for='expert'>Expert</label>"; */
-         echo "<p class='annonce__hs'>Nombre d'heure par séance: " . $niveau . "</p>"; 
-
+         echo "<p class='annonce__niveau'>Nombre d'heure par séance: " . $niveau . "</p>"; 
          echo "<p class='annonce__lieu'>Le lieu: " . $lieu . "</p>";
          echo "<p class='annonce__prix'>Prix: " . $prix . "</p>";
 
@@ -62,10 +39,25 @@
              <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
          <?php endif; ?>
          
-      
-         
       </article>
       <?php endif ?>
   
 </main>
 <?php get_footer() ?>
+
+<?php
+
+       /*   echo "<p>Type de formation: </p>";
+         echo " <input type='radio' id='css' name='a' value='css'> <label for='css'>CSS</label>";
+         echo " <input type='radio' id='php' name='a' value='php'> <label for='php'>PHP</label>";
+         echo " <input type='radio' id='js' name='a' value='js'> <label for='js'>JS</label>"; */
+
+      /*    echo "<p>Nombre d'heure par séance: </p>";
+         echo " <input type='radio' id='1' name='a' value='1'> <label for='1'>1h</label>";
+         echo " <input type='radio' id='2' name='a' value='2'> <label for='2'>2h</label>";
+         echo " <input type='radio' id='3' name='a' value='3'> <label for='3'>3h</label>"; */
+
+      /*   echo "<p>Niveau de difficulté: </p>";
+         echo " <input type='radio' id='debutant' name='a' value='debutant'> <label for='debutant'>Débutant</label>";
+         echo " <input type='radio' id='intermediaire' name='a' value='intermediaire'> <label for='intermediaire'>Intermédiaire</label>";
+         echo " <input type='radio' id='expert' name='a' value='expert'> <label for='expert'>Expert</label>"; */
